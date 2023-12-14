b48de174dd57abc0e53d0c060e2e738c8d48e0ca
dariacherepina <cherepinadara@gmail.com>
2023-12-14 20:28:55 +0100

    update branch

--- drones/src/drones/groupe_12/fra/uas/main.java
+++ drones/src/drones/groupe_12/fra/uas/main.java
@@ -1,8 +1,9 @@
 package drones.groupe_12.fra.uas;
-
 import java.net.HttpURLConnection;
-//import java.net.MalformedURLException;
 import java.net.URL;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+//import java.net.MalformedURLException;
 
 import org.json.JSONArray;
 
@@ -19,16 +20,31 @@ public class main {
 		 con.setRequestProperty("Authorization", TOKEN ) ;
 		 con.setRequestMethod("GET") ;
 		 con.setRequestProperty("User-Agent", "XYZ") ;
-		 int responseCode = con.getResponseCode() ;
-		 System.out.println("response code " + responseCode);
-		 JSONArray d;
+		 //int responseCode = con.getResponseCode() ;
+		 //System.out.println("response code " + responseCode);
+		// Get the response
+		 BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+		 String inputLine;
+		 StringBuffer content = new StringBuffer();
+		 while ((inputLine = in.readLine()) != null) {
+		    content.append(inputLine);
+		 }
+		 in.close();
+	
+	    // Parse the JSON response
+	   // ObjectMapper mapper = new ObjectMapper();
+	   // YourDataClass data = mapper.readValue(content.toString(), YourDataClass.class);
+	
+	    // Now you can use the data...
+	   // System.out.println("Data: " + data);
 		 
 		}
 		catch(Exception ex){
 			ex.getMessage();}
-		System.out.println("Das ist Daria!");
-	}
+		//System.out.println("Das ist Daria!");
+	
+	
+		}
+}
 	
 		
-
-}
